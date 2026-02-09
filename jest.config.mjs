d2b91521476198022c053779ef672a938e833598
Krystian <kr.olechowski@gmail.com>
2026-02-09 13:14:09 +0100

    test/setup-add-RTL-setup-for-Jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,6 +7,7 @@ const createJestConfig = nextJest({
 /** @type {import("jest").Config} */
 const config = {
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
